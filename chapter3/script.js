8b06704d00bc07b45db93266b1e8182d5e544caf
Paul <paul_grant@med.unc.edu>
2023-04-18 19:55:10 -0400

    update

---
+++ chapter3/script.js
@@ -0,0 +1,14 @@
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let catName = "Mr. Furley";
+console.log(catName);
+
+let orderTotal = 39.99;
+let itemPurchased = "JavaScript All-In-One For Dummies";
+let customerName = fullName;
+
+let thankYou = `${customerName}, thank you for your order of ${itemPurchased}. Your total is ${orderTotal}.`;
+console.log(thankYou);
---
+++ chapter5/script.js
@@ -0,0 +1,5 @@
+if (country === "United States" || country === "Liberia") {
+  temperature = temperature + "F";
+} else {
+  temperature = ((temperature - 32) * 5) / 9 + "C";
+}
